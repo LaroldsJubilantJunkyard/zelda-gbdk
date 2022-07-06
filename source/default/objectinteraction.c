e87854043ca5bb0af6c01467ae0713c3fdb1b32a
Larold <laroldsjubilantjunkyard@gmail.com>
2022-07-06 19:35:17 -0400

    changing where the text is drawn based on links position

--- source/default/objectinteraction.c
+++ source/default/objectinteraction.c
@@ -1,10 +1,12 @@
 #include "objects.h"
+#include "camera.h"
 #include "userinterface.h"
 
 uint8_t InteractWithObject(Object* object){
 
+    uint8_t drawAtTop = ((link->y>>4)-(cameraY>>4)) > (CAMERA_VERTICAL_SIZE/2);
 
-    DrawText("Your name is not zelda. Your name is in fact link. I dont get why",TRUE);
+    DrawText("Your name is not zelda. Your name is in fact link. I dont get why",drawAtTop);
 
     return 1;
 }
